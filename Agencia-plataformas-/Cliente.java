d7397be8e56d6d632d9e3cc348bc0871aa14937e
JuanVera11 <juanestebanv0612@gmail.com>
2026-04-25 14:44:11 -0500

    Agrego atributo direccion en cliente y clase ClienteVIP

---
+++ Agencia-plataformas-/Cliente.java
@@ -0,0 +1,41 @@
+public class Cliente {
+    protected String nombre;
+    protected String apellido;
+    protected String telefono;
+    protected String cedula;
+    protected String correo;
+    protected String direccion;
+
+    public Cliente(String nombre, String apellido, String telefono, String cedula, String correo, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.cedula = cedula;
+        this.correo = correo;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getApellido() { return apellido; }
+    public String getTelefono() { return telefono; }
+    public String getCedula() { return cedula; }
+    public String getDireccion() { return direccion; }
+
+    public String getNombreCompleto() {
+        return nombre + " " + apellido;
+    }
+
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + getNombreCompleto());
+        System.out.println("Telefono: " + telefono);
+    }
+
+    public void registraCliente() {
+        System.out.println("El cliente ha sido registrado exitosamente");
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Informacion del cliente:");
+        System.out.println(nombre + " " + apellido + " " + correo);
+    }
+}
